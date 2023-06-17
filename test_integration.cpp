641c5cde641040fb744176f16b397ac6c39a6416
Hao-yu Xie <112199186+HaoyuXie98@users.noreply.github.com>
2023-06-17 09:05:49 -0700

    Add files via upload

---
+++ test_integration.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "python_api_test.h"
+#include <map>
+#include <string>
+
+
+int main(int argc, char *argv[]) {
+    std::string name = "status", args = "";
+    std::map<std::string, std::string> ret1 = python_api(name, args);
+    // Output the key-value pairs of the ret to make sure 
+    for (const auto& pair : ret1) {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+    }
+
+    name = "move";
+    args = "--x 550 --y 70";
+    std::map<std::string, std::string> ret2 = python_api(name, args);
+    // Output the key-value pairs of the ret to make sure 
+    for (const auto& pair : ret2) {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+    }
+
+    name = "status";
+    args = "";
+    std::map<std::string, std::string> ret3 = python_api(name, args);
+    // Output the key-value pairs of the ret to make sure 
+    for (const auto& pair : ret3) {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+    }
+    return 0;
+}
