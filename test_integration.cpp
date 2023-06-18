de84c43e8a919004f39858889cc9ccbf278cb345
Hao-yu Xie <112199186+HaoyuXie98@users.noreply.github.com>
2023-06-17 20:02:49 -0700

    Update test_integration.cpp

--- test_integration.cpp
+++ test_integration.cpp
@@ -5,14 +5,14 @@
 
 
 int main(int argc, char *argv[]) {
-    std::string name = "status", args = "";
+    std::string name = "status.py", args = "";
     std::map<std::string, std::string> ret1 = python_api(name, args);
     // Output the key-value pairs of the ret to make sure 
     for (const auto& pair : ret1) {
         std::cout << pair.first << ": " << pair.second << std::endl;
     }
 
-    name = "move";
+    name = "move.py";
     args = "--x 550 --y 70";
     std::map<std::string, std::string> ret2 = python_api(name, args);
     // Output the key-value pairs of the ret to make sure 
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]) {
         std::cout << pair.first << ": " << pair.second << std::endl;
     }
 
-    name = "status";
+    name = "status.py";
     args = "";
     std::map<std::string, std::string> ret3 = python_api(name, args);
     // Output the key-value pairs of the ret to make sure 
